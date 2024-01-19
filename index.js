c1a5580cda1aa10fbbe181d549d5e7ccea5152d0
Dennis <ngetichdennis90@gmail.com>
2024-01-19 13:55:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  // Use the fetch API to make a GET request to the API endpoint
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response) {
+      // Check if the response is ok
+      if (response.ok) {
+        // Return the response as JSON
+        return response.json();
+      } else {
+        // Throw an error
+        throw new Error('Something went wrong');
+      }
+    })
+    .then(function(books) {
+      // Call the renderBooks function with the books data
+      renderBooks(books);
+    })
+    .catch(function(error) {
+      // Log the error to the console
+      console.error(error);
+    });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
